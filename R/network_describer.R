fbe13f23bb3fec8d9e5090f45f8d7d01c7774e57
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-16 20:54:04 +0100

    Update network_describer.R

--- R/network_describer.R
+++ R/network_describer.R
@@ -35,7 +35,9 @@ has been fully computed. But you can cancel the running process in case you want
 computation to be done. The networks can appear very empty when reducing the data through the use of filters. You adjust the minimum
 threshold using the advanced settings. However, note that setting values too low may result in very overcrowded plots. Hence, we only
 show a maximum of 2000 unique word combinations in a network plot. The absolute thresholds for minimum number of occurences are set to 10 while and the minimum
-correlation is set to 0.15. When choosing lower values the filters will default to the aforementioned values.
+correlation is set to 0.15. When choosing lower values the filters will default to the aforementioned values. When searching tweets of specific users
+the thresholds can be set lower as there will also be fewer tweets available. Minimum values here are 1 for occurences of words and bigrams while correlation
+still has to be at leastt 0.15.
 If the plot still becomes too overcrowded we recommend to first remove the network through the 'Remove Network' button in order to avoid lag.
 Then we recommend increasing the thresholds. Another option if the chosen network stemmed from word pairs is to switch to bigram networks
 as they tend be less overcrowded for the same parameters.
